ef2fdb3b76c8b5ff16262c8a7db458bf55ab00ab
Jamir Castro Araújo <jamir.c.a@hotmail.com>
2016-06-04 23:49:38 -0300

    teste

--- GraphCache/Convention/DefaultConvention.cs
+++ GraphCache/Convention/DefaultConvention.cs
@@ -7,7 +7,7 @@ namespace GraphCache.Conventions
         public override Func<object, string> CreateKeyExtractor(Type type)
         {
             var getter = GetPropertyGetter(type, "Id");
-            return value => getter(value).ToString();
+            return value => getter(value).ToString(); 
         }
 
         public override bool FitInConvention(Type type)
